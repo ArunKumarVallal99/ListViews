eac3605f2e6fe9214aefb729f36abefd6a9f4229
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-11-27 18:18:09 +0530

    FlatList with Class

--- App.js
+++ App.js
@@ -1,35 +1,63 @@
 import React from 'react';
-import {ScrollView, RefreshControl,StyleSheet,Text,} from 'react-native';
-
-const wait = (timeout) => {
-  return new Promise(resolve => {
-    setTimeout(resolve, timeout);
-  });
-}
-const App = () => {
-  const [refreshing, setRefreshing] = React.useState(false);
-  const onRefreshing = React.useCallback(() => {
-    setRefreshing(true);
-    wait(500).then(() => setRefreshing(false));
-  }, []);
+import {Alert, FlatList,StyleSheet,Text,View} from 'react-native';
+import { Component } from 'react';
+const itemSeparatorView = () => {
   return (
-    
-      <ScrollView
-        contentContainerStyle={styles.scrollView}
-        refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefreshing} />
-      }>
-        <Text>Pull down</Text>
-      </ScrollView>
-
+    //Item Separator
+    <View
+      style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}
+    />
   );
+};
+const getItem = (item) => {
+  //Function for click on an item
+  alert('Id : ' + item.id + ' Name : ' + item.name);
+};
+class YourApp extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      FlatListItems: [
+        {name:'Arun',id:'1'},
+        {name:'Shyam',id:'2'},
+        {name:'Einsten',id:'3'},
+        {name:'Helen',id:'4'},
+        {name:'FlatList',id:'5'},
+        {name:'sample',id:'6'}
+      ],
+    };
+  }
+  
+  render() {
+    return (
+      <FlatList
+        style={styles.flatLsit}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={itemSeparatorView}
+        data={this.state.FlatListItems}
+        renderItem={({item})=>(
+          <View style={styles.dataView}>
+            <Text style={styles.textInView} onPress={getItem(item)}> at{item.name}</Text>
+          </View>
+        )}
+      />
+    );
+  }
 }
+
 const styles = StyleSheet.create({
-  scrollView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  flatLsit:{
+    marginTop:40,
+  },
+  dataView:{
+    justifyContent:'center',
+    marginBottom:10,
+  },
+  textInView:{
+    backgroundColor:'blue',
+    color:'white',
+    padding:10,
   },
 });
 
-export default App;
+export default YourApp;
