c06c55980426adb96a489f662dedad3b52641d5a
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-11-30 16:25:08 +0530

    Data from Net

--- App.js
+++ App.js
@@ -1,48 +1,43 @@
-import React, { Component } from "react";
-import { FlatList, Text, View,StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-const Country = ({ item }) => (
-    <Text style={styles.item}>{item.title}</Text>
-);
+    this.state = {
+      data: [],
+      isLoading: true
+    };
+  }
 
-class App extends Component{
-    constructor(props) {
-        super(props);
-        this.state = { data:[ {title:'Germany'}, 
-            {title:"Australia"},
-             {title:"Sri Lanka"},
-             {title: "Japan"},
-             {title: "Austria"},
-             {title: "Spain"},
-             {title: "America"},
-             {title: "India"},
-             {title: "France"},
-             {title: "Belgium"},
-             {title: "China"},
-             {title: "Newzland"}]
-             
-        }; 
-    }
+  componentDidMount() {
+    fetch('https://reactnative.dev/movies.json')
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({ data: json.movies });
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+  }
 
-    render() {
-        return (
-           
-                <FlatList
-                    data={this.state.data}
-                    renderItem={Country}
-                />
-       
-        );
-    }
-}
+  render() {
+    const { data, isLoading } = this.state;
 
-const styles=StyleSheet.create({
-    item: {
-        backgroundColor: 'pink',
-        padding: 20,
-        marginVertical: 8,
-        marginHorizontal: 16,
-      },
-});  
-export default App;
+    return (
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text>{item.title}, {item.releaseYear}</Text>
+            )}
+          />
+        )}
+      </View>
+    );
+  }
+};
