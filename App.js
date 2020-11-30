4aa7b2317e49162346095b157ccb05fe0e5267b7
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-11-30 15:11:57 +0530

    FlatList with own setof data

--- App.js
+++ App.js
@@ -1,63 +1,48 @@
-import React from 'react';
-import {Alert, FlatList,StyleSheet,Text,View} from 'react-native';
-import { Component } from 'react';
-const itemSeparatorView = () => {
-  return (
-    //Item Separator
-    <View
-      style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}
-    />
-  );
-};
-const getItem = (item) => {
-  //Function for click on an item
-  alert('Id : ' + item.id + ' Name : ' + item.name);
-};
-class YourApp extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      FlatListItems: [
-        {name:'Arun',id:'1'},
-        {name:'Shyam',id:'2'},
-        {name:'Einsten',id:'3'},
-        {name:'Helen',id:'4'},
-        {name:'FlatList',id:'5'},
-        {name:'sample',id:'6'}
-      ],
-    };
-  }
-  
-  render() {
-    return (
-      <FlatList
-        style={styles.flatLsit}
-        keyExtractor={(item, index) => index.toString()}
-        ItemSeparatorComponent={itemSeparatorView}
-        data={this.state.FlatListItems}
-        renderItem={({item})=>(
-          <View style={styles.dataView}>
-            <Text style={styles.textInView} onPress={getItem(item)}> at{item.name}</Text>
-          </View>
-        )}
-      />
-    );
-  }
+import React, { Component } from "react";
+import { FlatList, Text, View,StyleSheet } from "react-native";
+
+
+const Country = ({ item }) => (
+    <Text style={styles.item}>{item.title}</Text>
+);
+
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { data:[ {title:'Germany'}, 
+            {title:"Australia"},
+             {title:"Sri Lanka"},
+             {title: "Japan"},
+             {title: "Austria"},
+             {title: "Spain"},
+             {title: "America"},
+             {title: "India"},
+             {title: "France"},
+             {title: "Belgium"},
+             {title: "China"},
+             {title: "Newzland"}]
+             
+        }; 
+    }
+
+    render() {
+        return (
+           
+                <FlatList
+                    data={this.state.data}
+                    renderItem={Country}
+                />
+       
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  flatLsit:{
-    marginTop:40,
-  },
-  dataView:{
-    justifyContent:'center',
-    marginBottom:10,
-  },
-  textInView:{
-    backgroundColor:'blue',
-    color:'white',
-    padding:10,
-  },
-});
-
-export default YourApp;
+const styles=StyleSheet.create({
+    item: {
+        backgroundColor: 'pink',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+      },
+});  
+export default App;
