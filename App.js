d8e2cae97b886dc9831807aaa8186c7444234144
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-02 19:41:53 +0530

    Pagination Demo with FlatList Component

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet,Image, ImageBackground } from 'react-native';
+import { View, Text, FlatList, StyleSheet,Image, ImageBackground, RefreshControl } from 'react-native';
 
 export default class App extends React.Component {
-  state = {
-    items: [
-      {id:'1',name:'Sam Curran',des:'When the image is resized, the corners of the size specified by capInsets will stay a fixed size.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago' },
-      {id:'2',name:'James Anderson',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.' ,imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-      {id:'3',name:'Sam Bilings',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-      {id:'4',name:'Sia',des:'Vivamus sollicitudin lacus sit amet rutrum placerat.Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-      {id:'5',name:'Taylor',des:'Nam sit amet elit vel odio porttitor posuere at dapibus diam. Duis vitae enim id ipsum laoreet porttitor a sit amet nunc.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-      {id:'6',name:'Alan Walker',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'}
-    ],
-  }
-
+        state={
+          firstSetData: [
+              {id:'1',name:'Sam Curran',des:'When the image is resized, the corners of the size specified by capInsets will stay a fixed size.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago' },
+              {id:'2',name:'James Anderson',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.' ,imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'3',name:'Sam Bilings',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'4',name:'Sia',des:'Vivamus sollicitudin lacus sit amet rutrum placerat.Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'5',name:'Taylor',des:'Nam sit amet elit vel odio porttitor posuere at dapibus diam. Duis vitae enim id ipsum laoreet porttitor a sit amet nunc.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'6',name:'Alan Walker',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'} 
+            ],
+            secondSetData:[
+              {id:'7',name:'Martin',des:'Vestibulum congue, odio id porttitor congue, orci lectus. Donec sed viverra nisi.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'8',name:'Natarajan',des:'When the image is resized, the corners of the size specified by capInsets will stay a fixed size.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago' },
+              {id:'9',name:'Gill',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.' ,imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'10',name:'Harry Kane',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'11',name:'Mia',des:'Vivamus sollicitudin lacus sit amet rutrum placerat.Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'12',name:'Annie',des:'Nam sit amet elit vel odio porttitor posuere at dapibus diam. Duis vitae enim id ipsum laoreet porttitor a sit amet nunc.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'13',name:'Moon Pie',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'14',name:'Hello World',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
+              {id:'15',name:'Son',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'}
+            ],
+            isRefreshing:false,
+            
+        }
   renderRow = ({ item }) => {
     return (
       <View style={styles.container}>
@@ -27,13 +39,42 @@ export default class App extends React.Component {
       </View>
     )
   }
+  endReached=()=>{
+    console.warn(this.state.firstSetData.length+this.state.secondSetData.length)
+      if(this.state.firstSetData.length<this.state.secondSetData.length){
+        // this.setState({
+        //   firstSetData:this.state.firstSetData.splice(this.state.firstSetData.length,0,this.state.secondSetData[this.state.firstSetData.length-6])
+        // })
+        this.setState({
+                firstSetData:this.state.firstSetData.concat(this.state.secondSetData)})
+        console.log(this.state.firstSetData)
+      } 
+    // //   console.warn(this.state.firstSetData.length)
+    // if(this.state.firstSetData.length<7){
+    //   console.warn(this.state.firstSetData.length)
+    //   this.setState({
+    //           firstSetData:this.state.firstSetData.concat(this.state.secondSetData)})
+    //   console.log(this.state.firstSetData.length)
+    // }
+   
+  }
+  refresh=()=>{
+    this.setState({isRefreshing:true,})
+  }
 
   render() {
     return (
       <View >
         <FlatList
-          data={this.state.items}
+          data={this.state.firstSetData}
           renderItem={this.renderRow}
+          keyExtractor={item=>item.id}
+          extraData={this.state.secondSetData}
+          onEndReached={this.endReached}
+          onEndReachedThreshold={0}
+          refreshControl={
+            <RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.refresh}/>
+          }
         />
       </View>
     )
