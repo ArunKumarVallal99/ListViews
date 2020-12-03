f4f6980722f97cc769d5bec0d0f93523c12ee8d5
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-03 17:55:35 +0530

    SectionList Demo

--- App.js
+++ App.js
@@ -1,120 +1,115 @@
 import React from 'react';
-import { View, Text, FlatList, StyleSheet,Image, ImageBackground, RefreshControl } from 'react-native';
+import { View, Text, FlatList, StyleSheet,Image, ImageBackground, RefreshControl, SectionList } from 'react-native';
 
-export default class App extends React.Component {
-        state={
-          firstSetData: [
-              {id:'1',name:'Sam Curran',des:'When the image is resized, the corners of the size specified by capInsets will stay a fixed size.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago' },
-              {id:'2',name:'James Anderson',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.' ,imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'3',name:'Sam Bilings',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'4',name:'Sia',des:'Vivamus sollicitudin lacus sit amet rutrum placerat.Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'5',name:'Taylor',des:'Nam sit amet elit vel odio porttitor posuere at dapibus diam. Duis vitae enim id ipsum laoreet porttitor a sit amet nunc.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'6',name:'Alan Walker',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'} 
-            ],
-            secondSetData:[
-              {id:'7',name:'Martin',des:'Vestibulum congue, odio id porttitor congue, orci lectus. Donec sed viverra nisi.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'8',name:'Natarajan',des:'When the image is resized, the corners of the size specified by capInsets will stay a fixed size.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago' },
-              {id:'9',name:'Gill',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.' ,imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'10',name:'Harry Kane',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'11',name:'Mia',des:'Vivamus sollicitudin lacus sit amet rutrum placerat.Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'12',name:'Annie',des:'Nam sit amet elit vel odio porttitor posuere at dapibus diam. Duis vitae enim id ipsum laoreet porttitor a sit amet nunc.',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'13',name:'Moon Pie',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'14',name:'Hello World',des:'Donec sed viverra nisi. Vestibulum congue, odio id porttitor congue, orci lectus',imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'15',name:'Son',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'},
-              {id:'16',name:'Newzland',des:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sollicitudin lacus sit amet rutrum placerat.', imageUri:'https://reactnative.dev/img/tiny_logo.png',hours:'2 hours ago'}
-            ],
-            isRefreshing:false,
-            arr:[]
-            
-        }
-  renderRow = ({ item }) => {
-    return (
-      <View style={styles.container}>
-        <View style={styles.imageHolder}>
-            <Image source={{uri:item.imageUri}} style={styles.imageContainer}/>   
-        </View>
-        <View style={styles.textContainer}>
-    <Text style={styles.title}>{item.name} ID:{item.id}</Text>
-            <Text style={styles.des}>{item.des}</Text>
-            <Text style={styles.hour}>{item.hours}</Text>
-        </View>
+const teamA=[
+  {id:1,name:"Sam Billings",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:2,name :"Sam Curran",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:3,name:"Maradona",imageUri:'https://reactnative.dev/img/tiny_logo.png'}
+];
+const teamB=[
+  {id:1,name:"James Anderson",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:2,name :"Tom Latham",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:3,name:"Pedri",imageUri:'https://reactnative.dev/img/tiny_logo.png'}
+];
+const teamC=[
+  {id:1,name:"Helen Eleven",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:2,name :"Annie Mariya",imageUri:'https://reactnative.dev/img/tiny_logo.png'}, 
+  {id:3,name:"Drake Sheldon",imageUri:'https://reactnative.dev/img/tiny_logo.png'}
+];
+const DATA= [
+  {
+    title: "Team A",
+    data:teamA
+  },
+  {
+    title: "Team B",
+    data:teamB
+  },
+  {
+    title: "Team C",
+    data:teamC
+  },
+  ];
+  const sym='>';
+  SectionListItems = ({ items }) => (
+    <View style={styles.container}>
+      <View style={styles.imageHolder}>
+        <Image source={{uri:items.imageUri}} style={styles.imageContainer}/>
       </View>
-    )
-  }
-
-  //Considering the inital load as 6 for a page and at reaching the end another 5 will be loaded.
-  endReached=()=>{         
-    console.log(this.state.firstSetData.length)                                                  
-    if(this.state.firstSetData.length<this.state.secondSetData.length+5){
-      this.setState({
-          firstSetData:this.state.firstSetData.concat(this.state.secondSetData.slice(this.state.firstSetData.length-6,this.state.firstSetData.length-1))
-        })
-      } 
-  }
-  refresh=()=>{
-    this.setState({isRefreshing:true})
-  }
-
+      <View style={styles.textHolder}>
+        <Text style={styles.text}>{items.name}</Text>
+      </View>  
+      <View style={styles.symbolContainer}>   
+        <Text style={styles.symbol}>{sym}</Text>
+      </View>
+    </View>
+  );
+export default class App extends React.Component {
+      
   render() {
     return (
       <View >
-        <FlatList
-          data={this.state.firstSetData}
-          renderItem={this.renderRow}
-          keyExtractor={item=>item.id}
-          extraData={this.state.secondSetData}
-          onEndReached={this.endReached}
-          onEndReachedThreshold={0}
-          refreshControl={
-            <RefreshControl refreshing={this.state.isRefreshing} onRefresh={this.refresh}/>
-          }
-        />
+        <SectionList
+          sections={DATA}
+          renderItem={({item})=><SectionListItems items={item}/>}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+         />
       </View>
     )
   }
 }
 const styles=StyleSheet.create({
+  header:{
+    paddingLeft:10,
+    fontSize:14,
+    paddingBottom:5,
+    fontWeight:'bold',
+  },
   container:{
     flexDirection:'row',
+    backgroundColor:'#ffffff',
+    margin:10,
+    overflow: 'hidden',
+    alignContent:'center',
+    elevation:7,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 5 },
+    shadowOpacity: 100,
+    shadowRadius: 10,
+    borderRadius:5,
   },
   imageHolder:{
-    padding:10,
-    alignItems:'center',
-   // backgroundColor:'pink',
-    borderBottomWidth:.5,
-    borderBottomColor:'grey',
-   // justifyContent:'center',
+    alignContent:'center',
+    justifyContent:'center',
+    marginLeft:20,
   },
   imageContainer:{
-    width:50, 
-    height:50,
-    padding:10,
-    borderRadius:50,
-    marginTop:10,
+    width:35,
+    height:35,
+    borderRadius:100,
+    
   },
-  textContainer:{
+  textHolder:{
+    paddingTop:20,
+    paddingBottom:20,
+    paddingRight:20,
     paddingLeft:10,
-    paddingRight:100,
-    borderBottomColor:'grey',
-    borderBottomWidth:.5,
-    //backgroundColor:'pink',
-    flexDirection:'column',
-    
+    flexDirection:'row',
+    width:170,
   },
-  title:{ 
-    paddingTop:10,
-    color:'#00b0ff',
-    fontSize:17,
-    justifyContent:'flex-start',
-    
+  text:{
+    fontSize:14,
   },
-  des:{
-    paddingBottom:5,
-    justifyContent:'space-evenly',
-    fontSize:15 ,
+  symbolContainer:{
+    marginLeft:110,
+    justifyContent:'center',
+    alignItems:'center',
   },
-  hour:{
-    paddingBottom:15,
+  symbol:{
+    fontSize:28,
     color:'grey',
   },
+  
 });
