22ba316fe6a72613ba7e7555e611b93a0af3c912
Arunkumarvallal99 <arunkumar.v@optisolbusiness.com>
2020-12-07 18:13:56 +0530

    Image PIcker

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import {View,Text,Image, StyleSheet,Button,Alert} from "react-native";
 import DateTimePicker from '@react-native-community/datetimepicker';
 import ImagePicker from 'react-native-image-picker'
 class ImagesPicker extends Component{
-state={
-  touched:false
-}
-
+  state={
+  
+  }
+  
 onPressed=()=>{
   const options = {
     title: 'Choose a Image',
@@ -36,7 +36,7 @@ render (){
   return(
     <View style={styles.container}>
       <Button title ='Image' onPress={this.onPressed}/> 
-      <Image source={this.state.imageSource} style={styles.uploadAvatar} />
+      <Image source={this.state.imageSource} style={styles.imageHolder} />
     </View>
     
   )
@@ -48,7 +48,7 @@ const styles= StyleSheet.create({
   container:{
 padding:80,
   },
-uploadAvatar:{
+imageHolder:{
   padding:20,
   margin:40,
   width:180,
